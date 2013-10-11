8b378dbb35d1f0a7c7346399a274c8b0d309dbd6
Marius Volkhart <Marius@volkhart.com>
2013-10-10 23:29:03 -0500

    some of the queries

---
+++ src/Queries.sql
@@ -0,0 +1,12 @@
+# Part 2
+SELECT last_name, first_name, phone_number FROM 'owner' ORDER BY last_name, first_name;
+
+# Part 3
+SELECT last_name, first_name FROM 'owner', 'owner_has_unit', 'unit' WHERE unit.name = ? AND
+                                                                              unit.number = ?
+GROUP BY last_name, first_name HAVING COUNT(DISTINCT week_number) >= ?;
+
+# Part 4 - Not Done
+SELECT last_name, first_name, other FROM 'owner', 'owner_has_unit', 'unit' WHERE unit.name = ? AND
+                                                                                 unit.number = ?
+  ORDER BY last_name, first_name;
