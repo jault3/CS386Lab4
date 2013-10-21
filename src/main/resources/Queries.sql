6aac2d6b8180f8ac3f6936d9b62cd05d51b26738
terpt <terpt@msoe.edu>
2013-10-21 00:58:44 -0500

    Update Queries.sql

--- src/main/resources/Queries.sql
+++ src/main/resources/Queries.sql
@@ -10,3 +10,33 @@ GROUP BY last_name, first_name HAVING COUNT(DISTINCT week_number) >= ?;
 SELECT last_name, first_name, other FROM 'owner', 'owner_has_unit', 'unit' WHERE unit.name = ? AND
                                                                                  unit.number = ?
   ORDER BY last_name, first_name;
+  
+# Part 5
+select o.last_name, o.first_name, count(*) weeks_owned
+from owner o, owner_has_unit u
+where o.id = u.owner_id
+     and u.unit_name = '?????'
+group by o.last_name having weeks_owned > 1
+order by o.last_name, o.first_name;
+
+# Part 6
+select ohu.unit_name,ohu.unit_number, ohu.week_number 
+from owner o, owner_has_unit ohu
+where o.last_name = '?????'
+     and o.id = ohu.owner_id
+order by ohu.unit_name, ohu.unit_number, ohu.week_number;
+
+#PART 7
+select o.last_name, o.first_name, ohu.week_number
+from owner o, owner_has_unit ohu
+where ohu.owner_id = o.id
+     and ohu.unit_name = '?????'
+     and ohu.unit_number = '?????'
+order by ohu.week_number;
+
+#PART 8
+select o.last_name, o.first_name, ohu.unit_name, ohu.unit_number
+from owner o, owner_has_unit ohu
+where ohu.owner_id = o.id
+     and ohu.week_number = '?????'
+order by ohu.unit_name, ohu.unit_number, o.last_name;
